cb90f12573b03c22c5ae16cb65cc538eeffc464b
ayooubsmatti <ayooubsmatti@gmail.com>
2020-01-03 15:20:06 +0100

    configuration loopback and Ospf

--- pythonR1Script.py
+++ pythonR1Script.py
@@ -1,19 +1,28 @@
 import getpass
+import sys
 import telnetlib
 
-HOST = "localhost"
-user = input("Enter your remote account: ")
+HOST = "192.168.62.136 "
+user = raw_input("Enter Username: ")
 password = getpass.getpass()
 
 tn = telnetlib.Telnet(HOST)
 
-tn.read_until(b"login: ")
-tn.write(user.encode('ascii') + b"\n")
+tn.read_until("Username: ")
+tn.write(user + "\n")
 if password:
-    tn.read_until(b"Password: ")
-    tn.write(password.encode('ascii') + b"\n")
+    tn.read_until("Password: ")
+    tn.write(password + "\n")
 
-tn.write(b"ls\n")
-tn.write(b"exit\n")
+tn.write("enable\n")
+tn.write("cisco\n")
+tn.write("conf t\n")
+tn.write("int loop 0\n")
+tn.write("ip address 1.1.1.1 255.255.255.255 \n")
+tn.write("router ospf 1 \n")
+tn.write("network 0.0.0.0 255.255.255.255\n")
+tn.write("end\n")
+tn.write("exit\n")
 
-print(tn.read_all().decode('ascii'))
+
+print tn.read_all()
